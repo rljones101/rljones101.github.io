84277517e23d643b83485ba77e66e7b2dd9bcbb4
Rick <rjones@digitalseat.com>
2023-07-15 11:56:14 -0500

    Added projects array. Updated buttons for projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,74 @@
 import "./App.css";
 import { HashLink } from "react-router-hash-link";
 
+// Source URLs
 const OPINION_RATE_IT_LINK =
   "https://gitfront.io/r/rljones101/RVLYyHZkZ835/opinionrateit-site/";
-const CAT_FACTS = "https://github.com/rljones101/cat-facts";
-const DS_METRICS_PORTAL =
-  "https://gitfront.io/r/rljones101/jXWsu2S5Aek9/ds-metrics-portal/";
-const APP_EDITOR = "";
-const UI_COMPONENT_LIBRARY = "";
-const L7_CHALLENGE = "https://github.com/rljones101/l7-challenge-2";
+const OPINION_RATE_IT_SITE = "https://dev.opinionrateit.com/";
+
+const projects = [
+  {
+    id: 1,
+    image: "videos.png",
+    name: "OpinionRateIt",
+    tech: ["Vue 3", "HTML", "Tailwind CSS", "Node", "Express", "MongoDB"],
+    description:
+      "Allows users to signup with their YouTube account. They can search for existing content from YouTube and add this to the application. It provides user feedback via metrics that other users enter and interact with. This was to created to give the reviewer insight on their reviement practice and adjust as needed.",
+    sourceUrl: OPINION_RATE_IT_LINK,
+    siteUrl: OPINION_RATE_IT_SITE,
+  },
+  {
+    id: 2,
+    image: "cat-facts.png",
+    name: "Cat Facts",
+    tech: ["React", "HTML", "Tailwind CSS"],
+    description:
+      "This is a fun approval system board that is similar to Jira. This was created in order to learn more about React development. It allows users to create quotes, approve or deny them. It also allows the user to sort each column by date.",
+    sourceUrl: "https://github.com/rljones101/cat-facts",
+    siteUrl: "https://cat-quotes-approval-system.netlify.app/",
+  },
+  {
+    id: 3,
+    image: "ds-client-portal.png",
+    name: "Client Portal",
+    tech: ["Vue 2", "HTML", "Tailwind CSS"],
+    description:
+      "This is a client portal that was created to allow users to login after events and view specific metrics for live events. Collect analysis based on a date range. Add and edit events. And also control the QR codes that are used during the events.",
+    sourceUrl:
+      "https://gitfront.io/r/rljones101/jXWsu2S5Aek9/ds-metrics-portal/",
+    siteUrl: "",
+  },
+  {
+    id: 4,
+    image: "app-editor.png",
+    name: "App Editor",
+    tech: ["Vue 2", "HTML", "Tailwind CSS"],
+    description:
+      "An editor that gave that provided users a way to create the live app experience at a given event. It allowed them to sort the content, update images, context and a 'call to action' that might open a URL for example.",
+    sourceUrl: "",
+    siteUrl: "",
+  },
+  {
+    id: 5,
+    image: "core-component-library.png",
+    name: "UI Component Library",
+    tech: ["Vue 2", "HTML", "CSS"],
+    description:
+      "This was a component library to use with mulitple projects that were developed while working at Digital Seat Media. It was created to speed up development time and unify the UI design between all projects.",
+    sourceUrl: "",
+    siteUrl: "",
+  },
+  {
+    id: 6,
+    image: "L7-challenge.png",
+    name: "L7 Challenge",
+    tech: ["Vue 3", "HTML", "Tailwind CSS"],
+    description:
+      "This was a UI design challenge to upload a file and then have it display the data via a chart. The data is displayed based on the column that the user chooses and against Benford's law analysis.",
+    sourceUrl: "https://github.com/rljones101/l7-challenge-2",
+    siteUrl: "",
+  },
+];
 
 function App() {
   return (
@@ -40,8 +100,8 @@ function AboutMeSection() {
   return (
     <section id="AboutMe" className="hero-background">
       <Nav />
-      <div className="flex-1 flex justify-center items-center p-8">
-        <div className="flex-1">
+      <div className="flex-1 flex flex-col md:flex-row justify-center items-center p-8">
+        <div className="order-2 md:order-1 flex-1">
           <p>Hello</p>
           <h2 className="font-bold text-3xl my-4 text-black">I'm Rick</h2>
           <p className="mb-4">
@@ -59,7 +119,7 @@ function AboutMeSection() {
             </Button>
           </div>
         </div>
-        <div className="flex-1 flex flex-col items-center justify-center relative">
+        <div className="order-1 md:order-2 flex-1 flex flex-col items-center justify-center relative">
           <img
             alt="Rick"
             src="profile-pic.jpg"
@@ -78,7 +138,7 @@ function AboutMeSection() {
 
 function ProficientSkills() {
   return (
-    <div className="flex bg-white text-slate-300 cursor-pointer text-xl w-full items-center justify-center gap-8 border border-slate-100  p-4">
+    <div className="flex flex-col md:flex-row bg-white text-slate-300 cursor-pointer text-xl w-full items-center justify-center gap-8 border border-slate-100  p-4">
       <p className="text-sm text-slate-300">Proficient in:</p>
 
       <span className=" hover:text-green-700 font-bold">
@@ -99,13 +159,13 @@ function FeaturedSiteSection() {
     <section className="bg-slate-100">
       <ProficientSkills />
       <div className="featured flex flex-col space-y-8 pt-8 pb-8 ">
-        <div className="overflow-hidden flex items-center gap-8 p-8">
-          <div className="flex-auto w-64 flex gap-4">
+        <div className="overflow-hidden flex flex-col md:flex-row items-center gap-8 p-8">
+          <div className="md:flex-auto md:w-64 flex gap-4">
             <div>
               <img src="site-screens.png" alt="OpinonRateIt" />
             </div>
           </div>
-          <div className="flex-auto w-32">
+          <div className="md:flex-auto md:w-32">
             <h2 className="uppercase font-bold mt-8 text-black">Featured</h2>
             <h3 className="text-2xl uppercase mb-4 text-blue-500">
               OpinionRateIt
@@ -117,7 +177,10 @@ function FeaturedSiteSection() {
               interact with. This was to created to give the reviewer insight on
               their reviement practice and adjust as needed.
             </p>
-            <Button link={OPINION_RATE_IT_LINK}>Code</Button>
+            <div className="flex gap-2">
+              <GitHubButton link={OPINION_RATE_IT_LINK}>Source</GitHubButton>
+              <Button link={OPINION_RATE_IT_SITE}>Website</Button>
+            </div>
           </div>
         </div>
       </div>
@@ -130,20 +193,20 @@ function PortfolioSection() {
     <section id="Portfolio">
       <Title className="mt-8">Portfolio</Title>
       <div className="grid grid-cols-1 md:grid-cols-2 p-8 ">
-        <Card
-          img="videos.png"
-          link={OPINION_RATE_IT_LINK}
-          title="OpinonRateIt"
-          tech="Vue 3,HTML,Tailwind CSS,Node,Express,MongoDB">
-          Allows users to signup with their YouTube account. They can search for
-          existing content from YouTube and add this to the application. It
-          provides user feedback via metrics that other users enter and interact
-          with. This was to created to give the reviewer insight on their
-          reviement practice and adjust as needed.
-        </Card>
-        <Card
+        {projects.map((project) => (
+          <Card
+            key={project.id}
+            img={project.image}
+            githubLink={project.sourceUrl}
+            siteLink={project.siteUrl}
+            title={project.name}
+            tech={project.tech}>
+            {project.description}
+          </Card>
+        ))}
+        {/* <Card
           img="cat-facts.png"
-          link={CAT_FACTS}
+          githubLink={CAT_FACTS}
           title="Cat Facts"
           tech="React,HTML,Tailwind CSS">
           This is a fun approval system board that is similar to Jira. This was
@@ -153,7 +216,7 @@ function PortfolioSection() {
         </Card>
         <Card
           img="ds-client-portal.png"
-          link={DS_METRICS_PORTAL}
+          githubLink={DS_METRICS_PORTAL}
           title="Client Portal"
           tech="Vue 2,HTML,Tailwind CSS">
           This is a client portal that was created to allow users to login after
@@ -163,7 +226,7 @@ function PortfolioSection() {
         </Card>
         <Card
           img="app-editor.png"
-          link={APP_EDITOR}
+          githubLink={APP_EDITOR}
           title="App Editor"
           tech="Vue 2,HTML,Tailwind CSS">
           An editor that gave that provided users a way to create the live app
@@ -173,7 +236,7 @@ function PortfolioSection() {
         </Card>
         <Card
           img="core-component-library.png"
-          link={UI_COMPONENT_LIBRARY}
+          githubLink={UI_COMPONENT_LIBRARY}
           title="UI Component Library"
           tech="Vue 2,HTML,CSS">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam quasi
@@ -182,13 +245,13 @@ function PortfolioSection() {
         </Card>
         <Card
           img="L7-challenge.png"
-          link={L7_CHALLENGE}
+          githubLink={L7_CHALLENGE}
           title="L7 Challenge"
           tech="Vue 3,HTML,Tailwind CSS">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam quasi
           mollitia eum itaque quam magni quas, odit voluptatum, omnis blanditiis
           labore beatae nobis ad dolorum repellat eveniet? Quis, est debitis.
-        </Card>
+        </Card> */}
       </div>
     </section>
   );
@@ -198,20 +261,54 @@ function SkillsSection() {
   return (
     <section id="Skills" className="flex flex-col justify-center">
       <Title>Skills</Title>
-      <div className="flex h-full items-stretch gap-8 z-10 p-8">
-        <CardSkills title="languages" skills="JavaScript,TypeScript,PHP" />
-        <CardSkills title="Frameworks" skills="Vue,React,AngularJS" />
-        <CardSkills title="Databases" skills="MongoDB,MySQL" />
-        <CardSkills title="Source Control" skills="Git,GitHub,JIRA" />
+      <div className="flex flex-col md:flex-row h-full items-stretch gap-8 z-10 p-8">
+        <CardSkills
+          title="languages"
+          skills="JavaScript,TypeScript,PHP"
+          icon="brands,square-js"
+        />
+        <CardSkills
+          title="Frameworks"
+          skills="Vue,React,AngularJS"
+          icon="brands,vuejs"
+        />
+        <CardSkills
+          title="Databases"
+          skills="MongoDB,MySQL"
+          customIcon="database"
+        />
+        <CardSkills
+          title="Source Control"
+          skills="Git,GitHub,JIRA"
+          icon="brands,github"
+        />
       </div>
     </section>
   );
 }
 
-function CardSkills({ title, skills }) {
+function CardSkills({ customIcon, icon, title, skills }) {
+  let iconArr = [];
+  let type = "";
+  let iconName = "";
+
+  if (icon) {
+    iconArr = icon.split(",");
+    [type, iconName] = iconArr;
+  }
+
   return (
-    <div className="flex-1 bg-white shadow-lg shadow-gray-500 p-8 border-t-4 border-black">
+    <div className="flex-1 bg-white shadow-lg shadow-gray-500 p-8 border-t-4 border-blue-500">
       <div>
+        {type && iconName && (
+          <i className={`fa-${type} fa-${iconName} text-3xl text-blue-500`}></i>
+        )}
+        {customIcon && (
+          <CustomIcon
+            icon={customIcon}
+            className="text-3xl fill-blue-500 block mb-1"
+          />
+        )}
         <h3 className="font-bold text-black text-lg">{title}</h3>
       </div>
       <ul>
@@ -223,11 +320,11 @@ function CardSkills({ title, skills }) {
   );
 }
 
-function Card({ img, tech, title, link, children }) {
+function Card({ img, tech, title, siteLink, githubLink, children }) {
   let technologySkills = [];
 
   if (tech) {
-    technologySkills = tech.split(",");
+    technologySkills = tech;
   }
 
   const techClass = (tech) => {
@@ -256,11 +353,7 @@ function Card({ img, tech, title, link, children }) {
         </div>
         <div className="card-content">
           <h2 className=" ">{title}</h2>
-          <div className="actions">
-            <Button link={link}>Code</Button>
-          </div>
           <div className="flex-1 h-full">
-            {children}
             <div className="technologies">
               {technologySkills.map((techItem) => (
                 <span key={techItem} className={`tech ${techClass(techItem)}`}>
@@ -268,6 +361,13 @@ function Card({ img, tech, title, link, children }) {
                 </span>
               ))}
             </div>
+            {children}
+          </div>
+          <div className="actions flex gap-2">
+            {githubLink && (
+              <GitHubButton link={githubLink}>Source</GitHubButton>
+            )}
+            {siteLink && <Button link={siteLink}>Website</Button>}
           </div>
         </div>
       </div>
@@ -282,15 +382,15 @@ function Nav() {
 
   return (
     <div className="flex w-full items-center justify-between self-start p-8 pb-0">
-      <div className="flex items-center gap-8 text-purple-950 bg-white">
-        <div className="bg-blue-500 rounded-full w-12 h-12 flex items-center justify-center text-white font-bold text-2xl ">
+      <div className="flex items-center gap-8 bg-white">
+        <div className="bg-blue-500 rounded-full w-12 h-12 hidden md:flex items-center justify-center text-white font-bold text-2xl ">
           RJ
         </div>
         <NavButton to="/#AboutMe">About Me</NavButton>
         <NavButton to="/#Portfolio">Portfolio</NavButton>
         <NavButton to="/#Skills">Skills</NavButton>
       </div>
-      <div className="flex gap-4 text-white text-2xl">
+      <div className="flex gap-4 text-blue-500 md:text-white text-2xl">
         <button
           onClick={() => openUrl("https://www.linkedin.com/in/rickljones/")}>
           <i className=" hover:text-slate-200 fa-brands fa-linkedin" />
@@ -317,6 +417,22 @@ function NavButton({ to, children }) {
   );
 }
 
+function GitHubButton({ link, children }) {
+  function openUrl() {
+    if (link) {
+      window.open(link, "mozillaTab");
+    }
+  }
+  return (
+    <button
+      className="bg-gradient-to-b from-gray-500 to-gray-700 hover:bg-gray-600 hover:scale-105 shadow-none hover:shadow-lg hover:shadow-gray-600 transition-all text-white py-2 px-4 uppercase rounded-lg button-code"
+      onClick={() => openUrl()}>
+      <i className=" hover:text-slate-200 fa-brands fa-github mr-1" />
+      {children}
+    </button>
+  );
+}
+
 function Button({ link, onClick, children }) {
   function openUrl() {
     if (link) {
@@ -330,7 +446,7 @@ function Button({ link, onClick, children }) {
 
   return (
     <button
-      className="bg-blue-500 hover:bg-blue-600  text-white py-2 px-4 uppercase rounded-lg"
+      className="bg-gradient-to-b from-blue-500 to-blue-700 hover:bg-blue-600 hover:scale-105 shadow-none hover:shadow-lg hover:shadow-gray-600 transition-all text-white py-2 px-4 uppercase rounded-lg button-code"
       onClick={() => openUrl()}>
       {children}
     </button>
@@ -343,6 +459,21 @@ function Title({ className, children }) {
       className={`text-center uppercase font-bold text-2xl text-black ${className}`}>
       {children}
     </h2>
+  );
+}
+
+function CustomIcon({ icon, className }) {
+  return (
+    <span className={className}>
+      {icon === "database" && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="1em"
+          viewBox="0 0 448 512">
+          <path d="M448 80v48c0 44.2-100.3 80-224 80S0 172.2 0 128V80C0 35.8 100.3 0 224 0S448 35.8 448 80zM393.2 214.7c20.8-7.4 39.9-16.9 54.8-28.6V288c0 44.2-100.3 80-224 80S0 332.2 0 288V186.1c14.9 11.8 34 21.2 54.8 28.6C99.7 230.7 159.5 240 224 240s124.3-9.3 169.2-25.3zM0 346.1c14.9 11.8 34 21.2 54.8 28.6C99.7 390.7 159.5 400 224 400s124.3-9.3 169.2-25.3c20.8-7.4 39.9-16.9 54.8-28.6V432c0 44.2-100.3 80-224 80S0 476.2 0 432V346.1z" />
+        </svg>
+      )}
+    </span>
   );
 }
 
