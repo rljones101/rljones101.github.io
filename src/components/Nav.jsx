e65c4d8bfe7e44fd1d41659f3ee6be7febe47de6
Rick <rjones@digitalseat.com>
2023-07-30 21:37:01 -0500

    Fixed nav layout for mobile.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ export default function Nav() {
   }
 
   return (
-    <div className="flex w-full items-center justify-between p-4 md:px-8 md:py-4 sticky top-0 bg-blue-50 z-50 shadow-lg ">
+    <div className="flex flex-col md:flex-row w-full items-center justify-between p-4 md:px-8 md:py-4 sticky top-0 bg-blue-50 z-50 shadow-lg ">
       <div className="flex items-center gap-8">
         <div className="bg-blue-500 rounded-full w-12 h-12 hidden md:flex items-center justify-center text-white font-bold text-2xl ">
           RJ
@@ -16,7 +16,7 @@ export default function Nav() {
         <NavButton to="/#Skills">Skills</NavButton>
         <NavButton to="/#Contact">Contact</NavButton>
       </div>
-      <div className="flex gap-4 text-blue-500  text-2xl">
+      <div className="flex gap-4 order-first md:order-last text-blue-500  text-2xl">
         <button
           onClick={() => openUrl("https://www.linkedin.com/in/rickljones/")}>
           <i className=" hover:text-blue-400 fa-brands fa-linkedin" />
