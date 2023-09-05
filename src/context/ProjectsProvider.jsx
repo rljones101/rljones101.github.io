a8ad8d96df4821c1e6aeda36b0959a7c1bd46dd7
Rick <rjones@digitalseat.com>
2023-09-05 12:02:24 -0500

    Added the source URL for the components library

--- src/context/ProjectsProvider.jsx
+++ src/context/ProjectsProvider.jsx
@@ -53,7 +53,7 @@ const projects = [
     tech: ["Vue 2", "HTML", "CSS"],
     description:
       "This was a component library to use with mulitple projects that were developed while working at Digital Seat Media. It was created to speed up development time and unify the UI design between all projects.",
-    sourceUrl: "",
+    sourceUrl: "https://github.com/rljones101/ds-core-ui-components",
     siteUrl: "",
   },
   {
